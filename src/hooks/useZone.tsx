8e586ff215a16367f116a8f3c45d5fd5ca1c57c4
MathildeIgier <44711385+MathildeIgier@users.noreply.github.com>
2022-10-28 14:56:16 +0200

    feat(screenshot): manage img smaller than 400px

--- src/hooks/useZone.tsx
+++ src/hooks/useZone.tsx
@@ -1,10 +1,9 @@
 import { useAppSelector } from "../app/hooks";
 import { Image as ChakraImage, Box } from "@chakra-ui/react";
+import { useState } from "react";
 
 //GET image loaded into project
-//CASE svg -> use parsing from branch figma-link to get correct content
-
-//CASE img -> S3 url
+//Link project to S3 url -> one img/ project
 
 export const useZone = () => {
   const zones = useAppSelector((state) => state.zones);
@@ -18,6 +17,7 @@ export const useZone = () => {
   };
 
   const ZoneScreenshot = ({ zoneId }: ZoneScreenshotProps) => {
+    const [imgWidth, setImgWidth] = useState(400);
     const zone = getZoneById(zoneId);
     if (!zone) return <div />;
 
@@ -25,18 +25,25 @@ export const useZone = () => {
     const yPosition = zone.y - 24 > 0 ? zone.y - 24 : 0;
     const width = zone.width > 0 ? zone.width : 0;
     const height = zone.height > 0 ? zone.height : 0;
-    //if image
 
-    //400 is the max size of image in ZoneView
-    //TODO: manage case < 400 -> see what happen
-    const url = `//images.weserv.nl/?url=https://bytesim-bucket.s3.eu-west-3.amazonaws.com/RE-about.jpg&w=400&cx=${xPosition}&cy=${yPosition}&cw=${width}&ch=${height}"`;
-    console.log(url);
-    //if svg
+    const imageUrl =
+      "https://bytesim-bucket.s3.eu-west-3.amazonaws.com/test.svg";
+    // const imageUrl =
+    //   "https://bytesim-bucket.s3.eu-west-3.amazonaws.com/RE-about.jpg";
+
+    const img = new Image();
+    img.src = imageUrl;
+
+    img.onload = () => {
+      if (img.width < 400) {
+        setImgWidth(img.width);
+      }
+    };
 
     return (
       <Box py={2}>
         <ChakraImage
-          src={url}
+          src={`//images.weserv.nl/?url=${imageUrl}&w=${imgWidth}&cx=${xPosition}&cy=${yPosition}&cw=${width}&ch=${height}"`}
           height="80px" /* fallbackSrc="/assets/placeholder.gif" */
         />
       </Box>
