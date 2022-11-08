ee3ef8a5bb459ca7334e36c26f960e75ed089a77
MathildeIgier <44711385+MathildeIgier@users.noreply.github.com>
2022-11-08 12:47:45 +0100

    Merge fix-31-group-zone
    Add:
    - Groupe recommandations by zones

--- src/components/recommandations/RecommandationsList.tsx
+++ src/components/recommandations/RecommandationsList.tsx
@@ -3,21 +3,47 @@ import { GenericParameters } from "../../app/types/generalFormTypes";
 import { RecommandationWithZone } from "../../app/types/recommandations";
 import { VideoParameters } from "../../app/types/videoTypes";
 import RecommandationDisplay from "./Recommandation";
+import RecommandationsByZone from "./RecommandationsByZone";
+
+export type RecommandationType = RecommandationWithZone<
+  | VideoParameters[keyof VideoParameters]
+  | GenericParameters[keyof GenericParameters]
+>;
 
 interface RecommandationsListProps {
-  recommandations: RecommandationWithZone<VideoParameters[keyof VideoParameters] | GenericParameters[keyof GenericParameters]>[];
+  recommandations: RecommandationType[];
   allOpen: boolean;
 }
 export default function RecommandationsList({
   recommandations,
-  allOpen
+  allOpen,
 }: RecommandationsListProps) {
   const indexes = Array.from(recommandations.keys());
+
+  const groupBy = (array: any[], property: any) =>
+    array.reduce(
+      (grouped, element) => ({
+        ...grouped,
+        [element[property]]: [...(grouped[element[property]] || []), element],
+      }),
+      {}
+    );
+
+  const recoGroupedByZone = (recommandations: RecommandationType[]) => {
+    return groupBy(recommandations, "zoneId");
+  };
+
   return (
     <Accordion allowToggle index={allOpen ? indexes : undefined}>
-      {recommandations.map((reco) => (
-        <RecommandationDisplay key={reco.id} recommandation={reco} />
-      ))}
+      {Object.entries(recoGroupedByZone(recommandations))?.map(
+        ([key, value]) => (
+          <RecommandationsByZone
+            key={key}
+            zoneRecommandations={value as RecommandationType[]}
+            zoneId={key}
+          />
+        )
+      )}
     </Accordion>
   );
 }
