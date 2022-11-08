81862ec2f5162b92cabcbfd8faa413fe87aec5cf
MathildeIgier <44711385+MathildeIgier@users.noreply.github.com>
2022-11-08 12:47:45 +0100

    feat(31): group zone

--- src/components/recommandations/Recommandation.tsx
+++ src/components/recommandations/Recommandation.tsx
@@ -1,10 +1,6 @@
 import {
-  AccordionButton,
-  AccordionItem,
-  AccordionPanel,
   Box,
   Flex,
-  Heading,
   Radio,
   RadioGroup,
   Stack,
@@ -13,22 +9,12 @@ import {
 } from "@chakra-ui/react";
 import React from "react";
 import { useDispatch } from "react-redux";
-import { GenericParameters } from "../../app/types/generalFormTypes";
-import {
-  RecommandationOption,
-  RecommandationWithZone,
-} from "../../app/types/recommandations";
-import { VideoParameters } from "../../app/types/videoTypes";
+import { RecommandationOption } from "../../app/types/recommandations";
 import { recommandationUpdated } from "../../features/recommandations/recommandationsSlice";
-import useZoneScreenshot from "../../hooks/useZoneScreenshot";
-import AccordionChevron from "../layout/AccordionChevron";
-//import { ReportCTX } from "./RecoReport";
+import { RecommandationType } from "./RecommandationsList";
 
 interface RecommandationDisplayProps {
-  recommandation: RecommandationWithZone<
-    | VideoParameters[keyof VideoParameters]
-    | GenericParameters[keyof GenericParameters]
-  >;
+  recommandation: RecommandationType;
 }
 
 export default function RecommandationDisplay({
@@ -36,7 +22,6 @@ export default function RecommandationDisplay({
 }: RecommandationDisplayProps) {
   //const { totalBenefits, setTotalBenefits } = React.useContext(ReportCTX);
   const dispatch = useDispatch();
-  const { ZoneScreenshot } = useZoneScreenshot();
 
   const onChangeParams = React.useCallback(
     (v: RecommandationOption) => {
@@ -46,47 +31,35 @@ export default function RecommandationDisplay({
     },
     [recommandation]
   );
-  return recommandation.betterValue ? (
-    <AccordionItem>
-      {({ isExpanded }) => (
-        <>
-          <AccordionButton _hover={{ backgroundColor: "brand.100" }} pl={2}>
-            <Box flex="1" textAlign="left">
-              <Heading mr={1} size="sm">
-                {recommandation.zoneName} - {recommandation.id}
-              </Heading>
-              <ZoneScreenshot zoneId={recommandation.zoneId} />
-              <Flex>
-                <Text>{recommandation.parameter}</Text>
-                <Tooltip label="Biblio de reco + best practices" hasArrow>
-                  ⓘ
-                </Tooltip>
-              </Flex>
-            </Box>
-            <AccordionChevron isExpanded={isExpanded} />
-          </AccordionButton>
-          <AccordionPanel>
-            <RadioGroup
-              value={recommandation.selectedValue || "current"}
-              onChange={onChangeParams}
-            >
-              <Stack>
-                <Radio colorScheme={"brand"} value={"current"} size="sm">
-                  Current:&nbsp;{recommandation.currentValue}
-                </Radio>
-                <Radio colorScheme={"brand"} value={"better"} size="sm">
-                  More sober:&nbsp;{recommandation.betterValue}
-                </Radio>
-                {recommandation.bestValue && (
-                  <Radio colorScheme={"brand"} value={"optimal"} size="sm">
-                    Most sober:&nbsp;{recommandation.bestValue}
-                  </Radio>
-                )}
-              </Stack>
-            </RadioGroup>
-          </AccordionPanel>
-        </>
-      )}
-    </AccordionItem>
-  ) : null;
+
+  return (
+    <Box mb={2}>
+      <Box flex="1" textAlign="left">
+        <Flex>
+          <Text>{recommandation.parameter}</Text>
+          <Tooltip label="Biblio de reco + best practices" hasArrow>
+            ⓘ
+          </Tooltip>
+        </Flex>
+      </Box>
+      <RadioGroup
+        value={recommandation.selectedValue || "current"}
+        onChange={onChangeParams}
+      >
+        <Stack>
+          <Radio colorScheme={"brand"} value={"current"} size="sm">
+            Current:&nbsp;{recommandation.currentValue}
+          </Radio>
+          <Radio colorScheme={"brand"} value={"better"} size="sm">
+            More sober:&nbsp;{recommandation.betterValue}
+          </Radio>
+          {recommandation.bestValue && (
+            <Radio colorScheme={"brand"} value={"optimal"} size="sm">
+              Most sober:&nbsp;{recommandation.bestValue}
+            </Radio>
+          )}
+        </Stack>
+      </RadioGroup>
+    </Box>
+  );
 }
