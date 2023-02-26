06dd1c5195ff618db913701881e5f08e2dd65e5f
GaspardC <chevassusgaspard@gmail.com>
2023-02-26 12:51:06 +0100

    wdyr

--- config-overrides.js
+++ config-overrides.js
@@ -1,3 +1,17 @@
-module.exports = function override(config, env) {
+const { override, getBabelLoader } = require("customize-cra");
+
+module.exports = override((config) => {
+  //@ts-ignore
+  const options = getBabelLoader(config, false).options;
+
+  const originalPreset = options.presets.find((preset) =>
+    preset[0].includes("babel-preset-react-app")
+  );
+  if (originalPreset) {
+    Object.assign(originalPreset[1], {
+      development: true,
+      importSource: "@welldone-software/why-did-you-render",
+    });
+  }
   return config;
-};
+});
