c2f2f640c7dfc3fe2e298c641d9893b9fdd91b2e
Stéphane Lecorney <stephane.lecorney@heig-vd.ch>
2023-02-28 16:34:23 +0100

    use AWS API to generate s3 presigned URL

--- src/components/project/UploadButton.tsx
+++ src/components/project/UploadButton.tsx
@@ -15,7 +15,6 @@ import shortId from "short-uuid";
 
 import { useNavigate } from "react-router-dom";
 import { setIsNew } from "../../features/zones/zonesSlice";
-import { ReactS3Client } from "../../utils/s3Config";
 import { ReactComponent as ImportIcon } from "../../assets/Import.svg";
 import Dropzone from "../layout/Dropzone";
 import ButtonWithIconCustom from "../layout/ButtonWithIconCustom";
@@ -64,29 +63,38 @@ export default function UploadButton() {
             <Button
               variant="solid"
               colorScheme={"brand"}
-              onClick={() => {
+              onClick={async () => {
                 // create file name with key and extension + ensure no special characters
                 const fileNameParts = fileName.split(".");
-                ReactS3Client.uploadFile(
-                  file,
-                  `${shortId.generate()}_${fileNameParts[0].replace(
-                    /[^a-zA-Z0-9 ]/g,
-                    ""
-                  )}.${fileNameParts.pop()}`
-                )
-                  .then((data: any) => {
-                    onClose();
-                    navigate(
-                      `/figma?key=${encodeURIComponent(data.key)}&new=true`
-                    );
-                    setFile(undefined);
-                    setFileName("");
-                    dispatch(setIsNew(true));
-                  })
-                  .catch((err: any) => {
-                    setErrorMessage("Error while uploading the image");
-                    console.log(err);
-                  });
+                const key = `${shortId.generate()}_${fileNameParts[0].replace(
+                  /[^a-zA-Z0-9 ]/g,
+                  ""
+                )}.${fileNameParts.pop()}`;
+                const type = (file as File).type;
+                // Generate a sign URL to upload to s3 bucket
+                // Based on https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/modules/_aws_sdk_s3_presigned_post.html
+                const response = await fetch(new URL(`${process.env.REACT_APP_S3_UPLOAD_URL_GENERATOR}?key=${key}&contentType=${encodeURIComponent(type)}`));
+                const presigned = await response.json() as { uploadURL: string, fields: { [key: string] : string}};
+
+                const fd = new FormData();
+                for (const field in presigned.fields) {
+                  fd.append(field, presigned.fields[field]);
+                }
+                fd.append("file", file as File);
+
+                const data = await fetch(presigned.uploadURL, { method: "post", body: fd });
+                if (data.ok) {
+                  onClose();
+                  navigate(
+                    `/figma?key=${encodeURIComponent(key)}&new=true`
+                  );
+                  setFile(undefined);
+                  setFileName("");
+                  dispatch(setIsNew(true));
+                } else {
+                  setErrorMessage("Error while uploading the image");
+                  console.log(data);
+                }
               }}
             >
               OK
--- src/utils/s3Config.ts
+++
@@ -1,9 +0,0 @@
-import S3 from "react-aws-s3";
-
-const config = {
-  bucketName: process.env.REACT_APP_S3_BUCKET,
-  region: process.env.REACT_APP_S3_REGION,
-  accessKeyId: process.env.REACT_APP_ACCESS_KEY,
-  secretAccessKey: process.env.REACT_APP_SECRET_KEY,
-};
-export const ReactS3Client = new S3(config);
